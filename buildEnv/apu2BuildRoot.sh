b8671d1b30f31c80cbf994d20c7eae5adf2ecc92
Aleem <5023467+ByteAndNibble@users.noreply.github.com>
2022-09-03 11:57:21 +0100

    add repo

--- buildEnv/apu2BuildRoot.sh
+++ buildEnv/apu2BuildRoot.sh
@@ -47,6 +47,10 @@ cd "$DIR" || exit
 mkdir -p files/etc/config
 cp ../files/* files/etc/config/
 
+### add repo to repositories.conf
+sed -i '/stangri_repo/d' repositories.conf
+! grep -q 'stangri_repo' repositories.conf && sed -i '2 i\src/gz stangri_repo repo.openwrt.melmac.net' repositories.conf
+    
 ### make!
 make clean
 make image PACKAGES="luci luci-ssl luci-theme-openwrt-2020 -dnsmasq dnsmasq-full ipset libnettle8 libnetfilter-conntrack3 kmod-gpio-button-hotplug kmod-crypto-hw-ccp kmod-leds-gpio kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-gpio-nct5104d kmod-pcspkr kmod-usb-core kmod-sound-core kmod-ipt-nat6 fstrim irqbalance amd64-microcode flashrom luci-app-sqm luci-app-wireguard luci-proto-wireguard qrencode stubby unbound-daemon luci-app-unbound https-dns-proxy luci-app-https-dns-proxy watchcat luci-app-watchcat pbr luci-app-pbr curl wget tcpdump luci-app-wol 6in4 6to4 6rd usbutils usb-modeswitch kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-ether comgt-ncm kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan luci-proto-ncm luci-proto-3g banip luci-app-banip avahi-dbus-daemon avahi-utils smcroute zerotier" EXTRA_IMAGE_NAME="apu2_2nic_byteandnibble" FILES=files/
