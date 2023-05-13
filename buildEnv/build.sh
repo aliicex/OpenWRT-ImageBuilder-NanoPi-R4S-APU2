d561be95171c1f8987559f43aeafbb453871e141
Llama-Alarm <5023467+Llama-Alarm@users.noreply.github.com>
2023-05-13 16:08:32 +0100

    use wolfssl

--- buildEnv/build.sh
+++ buildEnv/build.sh
@@ -67,7 +67,7 @@ cd "$DIR" || exit
 
 BANIP='banip luci-app-banip'
 DNSMASQFULL='-dnsmasq dnsmasq-full ipset libnettle8 libnetfilter-conntrack3'
-BATMAN='kmod-batman-adv batctl wpad-mesh-openssl -wpad-mini'
+BATMAN='kmod-batman-adv batctl wpad-mesh-wolfssl -wpad-mini'
 
 # luci-proto-batman-adv currently only available in SNAPSHOT
 rm packages/*
